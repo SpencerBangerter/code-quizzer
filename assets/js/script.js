c6a76a4bbc0b673138e0d05641d87f44c5f011a5
Spencer Bangterer <bangerter.spencer@gmail.com>
2019-12-09 00:03:34 -0700

    Set display question function and reset display function with appropriate variables needex

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,9 @@
 
 // let startButtonEl = $("#start-btn");
 let startButtonEl = document.getElementById('start-btn')
-
 let qContainerEl = document.getElementById('question-container');
 let questionEl = document.getElementById('question')
+let aGridEl = document.getElementById('answer-buttons')
 
 
 //When Start is clicked hide start and launch question 1.
@@ -14,15 +14,34 @@ startButtonEl.addEventListener('click', startQuiz);
 
 // Question index to increment current question
 let questionIndex = 0
-
+let currentQ = questionsArr[questionIndex]
 // Functions
 function startQuiz() {
     console.log('Start Quiz');
     startButtonEl.classList.add('hide');
     qContainerEl.classList.remove('hide');
-    dispQuestion()
+    dispQuestion(currentQ)
 }
 
-function dispQuestion() {
-    questionEl.textContent = questionsArr[questionIndex].title
+//Display the question of current index, and make buttons for each answer option. 
+//Allows for any number of answers on any question.
+function dispQuestion(question) {
+    //Removes previous button options
+    resetDisp()
+    //Set question text
+    questionEl.textContent = question.title
+    //create answer buttons for each item in answers choice array
+    question.choices.forEach(function (options) {
+        //create button, set text, add button class, and append to end for each choice
+        let newButton = document.createElement('button')
+        newButton.textContent = options
+        newButton.classList.add('btn')
+        aGridEl.appendChild(newButton)
+    })
+}
+
+function resetDisp() {
+    while (aGridEl.firstChild) {
+        aGridEl.removeChild(aGridEl.firstChild)
+    }
 }

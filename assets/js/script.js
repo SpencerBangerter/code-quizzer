236c79cc70545f3c90e60aa53ae7bd44dfdb22a7
Spencer Bangterer <bangerter.spencer@gmail.com>
2019-12-09 22:41:56 -0700

    Reformat code, and set up next question and select answer functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,30 @@
-///////////////////////
-// Element Variables //
-///////////////////////
+///////////////
+// Variables //
+///////////////
 
+// Element variables
 let startButtonEl = $("#start-btn");
-// let startButtonEl = document.getElementById('start-btn')
 let qContainerEl = $('#question-container');
 let questionEl = $('#question')
 let aGridEl = $('#answer-buttons')
-
-
-//When Start is clicked hide start and launch question 1.
-
-startButtonEl.click(startQuiz);
-// startButtonEl.addEventListener('click', startQuiz);
-
+let nextBtnEl = $('#next-btn')
+let answerBtnEl = $('#answer-buttons')
 // Question index to increment current question when needed
 let questionIndex = 0
 // sets question from question array based upon the index above
 let currentQ = questionsArr[questionIndex]
+let time = 300
+/////////////////////
+// Click Listeners //
+/////////////////////
+
+//When Start is clicked hide start and launch question 1.
+startButtonEl.click(startQuiz);
+
+//when an answer is selected run function
+answerBtnEl.click(selectAnswer);
+
+nextBtnEl.click(nextQuestion)
 
 ///////////////
 // Functions //
@@ -41,7 +48,6 @@ function dispQuestion(question) {
     questionEl.text(question.title);
     //create answer buttons for each item in answers choice array
     $.each(question.choices, function (index, options) {
-        debugger
         //create button, set text, add button class, and append to end for each choice
         let newButton = $('<button>');
         newButton.text(options);
@@ -49,4 +55,28 @@ function dispQuestion(question) {
         aGridEl.append(newButton);
     })
 }
+
+function nextQuestion () {
+    questionIndex++
+    currentQ = questionsArr[questionIndex]
+    if (questionIndex === questionsArr.length){
+        alert('Last Q')
+        questionEl.empty();
+        aGridEl.empty();
+    } else {
+        dispQuestion(currentQ)
+    }
+}
+//Bug when clicking inbetween buttons TO FIX
+function selectAnswer(e) {
+     if ($(e.target).text() === currentQ.answer) {
+         $(e.target).addClass('btn-success');
+         nextBtnEl.removeClass('hide');
+     } else if ($(e.target).text() !== currentQ.answer){
+        $(e.target).addClass('btn-danger');
+        time -= 15
+        console.log(time)
+     }
+}
+
 
